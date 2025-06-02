6e6f502bb88408cb9c840bfe734e417b0cfdcc1c
ankitjt <ankitjeetthakur@gmail.com>
2025-06-02 22:42:46 +0530

    logic and UI improvements

--- script.js
+++ script.js
@@ -1,19 +1,63 @@
-const copyCode = document.querySelectorAll(".copyCode")
+const copyCode = document.querySelectorAll( ".copyCode" )
 
-copyCode.forEach(copy => {
-  copy.addEventListener("click", () => {
-    let targetNode = copy.parentElement.parentElement.parentElement.children[ 1 ].children[ 0 ]
-    let toolTip = copy.parentElement.parentElement.children[ 1 ]
+copyCode.forEach( copyIcon =>
+{
+  copyIcon.addEventListener( "click", () =>
+  {
+    const parentComponent = copyIcon.closest( ".component" )
+    const targetNode = parentComponent.querySelector( ".wrapper .ui" )
+    const toolTip = parentComponent.querySelector( ".toolTip" )
 
-    setTimeout(() => {
+    setTimeout( () =>
+    {
       toolTip.innerHTML = `Code Copied !!`
-      copy.classList.add("bg-emerald-600", "text-slate-200")
-    }, 100)
-    setTimeout(() => {
+      toolTip.classList.remove( "hidden" )
+      copyIcon.classList.add( "bg-emerald-600", "text-slate-200" )
+    }, 100 )
+    setTimeout( () =>
+    {
       toolTip.innerHTML = ""
-      copy.classList.remove("bg-emerald-600", "text-slate-200")
-    }, 2000)
+      toolTip.classList.add( "hidden" )
+      copyIcon.classList.remove( "bg-emerald-600", "text-slate-200" )
+    }, 2000 )
 
-    navigator.clipboard.writeText(targetNode.outerHTML)
-  })
-})
+    navigator.clipboard.writeText( targetNode.outerHTML )
+  } )
+} )
+
+function formatHTML ( html )
+{
+  const tab = '  ' // 2 spaces
+  let result = ''
+  let indentLevel = 0
+
+  html.split( />\s*</ ).forEach( ( element, i ) =>
+  {
+    if ( element.match( /^\/\w/ ) ) indentLevel--
+
+    result += `${ tab.repeat( indentLevel ) }<${ element }>\n`
+
+    if ( element.match( /^<?\w[^>]*[^\/]$/ ) && !element.startsWith( "input" ) ) indentLevel++
+  } )
+
+  return result.trim()
+}
+
+
+const testBtn = document.querySelector( ".testBtn" )
+testBtn.addEventListener( "click", () =>
+{
+  const parent = testBtn.closest( ".component" )
+  const section1 = parent.querySelector( ".section1" )
+  const section2 = parent.querySelector( ".section2" )
+  section2.classList.toggle( "-right-[100vw]" )
+  section2.classList.toggle( "right-0" )
+  navigator.clipboard.writeText( section1.outerHTML )
+
+  let copiedText = section1.outerHTML
+  let formattedHTML = formatHTML( copiedText )
+  const codeBlock = parent.querySelector( ".userCode code" )
+  codeBlock.textContent = formattedHTML
+
+  Prism.highlightElement( codeBlock )
+} )
