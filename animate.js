c76e88d70f6815c756b0025cefd0354c6109bfd8
ankitjt <ankitjeetthakur@gmail.com>
2025-06-10 23:28:29 +0530

    new components added

--- animate.js
+++ animate.js
@@ -1,1 +1,50 @@
-console.log("test123")
+const myButton = document.querySelector( ".myButton" )
+const initial = document.querySelector( ".initial" )
+const loadingDots = document.querySelector( ".loadingDots" )
+const success = document.querySelector( ".success" )
+let isAnimating = false
+
+const show = el =>
+{
+  el.classList.remove( "hidden" )
+  setTimeout( () =>
+  {
+    el.classList.add( "flex" )
+    el.classList.remove( "opacity-0", "scale-75" )
+    el.classList.add( "opacity-100", "scale-100" )
+  }, 10 )
+}
+
+const hide = el =>
+{
+  el.classList.remove( "opacity-100", "scale-100" )
+  el.classList.add( "opacity-0", "scale-75" )
+  setTimeout( () =>
+  {
+    el.classList.remove( "flex" )
+    el.classList.add( "hidden" )
+  }, 700 )
+}
+
+myButton.addEventListener( "click", () =>
+{
+  if ( isAnimating ) return
+  isAnimating = true
+  hide( initial )
+  show( loadingDots )
+
+  setTimeout( () =>
+  {
+    hide( loadingDots )
+    show( success )
+
+    setTimeout( () =>
+    {
+      hide( success )
+      show( initial )
+      isAnimating = false
+
+    }, 5000 )
+  }, 3000 )
+} )
+
